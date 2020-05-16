7d28301b88833174b7ef5631fffc2fc586049d8d
SergeyMell <sergey.mell@agilie.com>
2020-05-16 22:59:44 +0300

    Update publishing process

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e # Stop running on exception
+
+# Update package version
+echo 'Put version [<newversion> | major | minor | patch | premajor | preminor | prepatch ]'
+read VERSION
+VERSION="${VERSION:-minor}" # defaults to minor
+npm version $VERSION
+
+# Running tests and building the project
+ng test --no-watch --code-coverage
+ng build
+
+# Send coverage report to codecov for github badge
+bash <(curl -s https://codecov.io/bash) -t f27c863b-d1a1-4e34-a7cf-5c4559de19f3
+
+# Publishing the project from dist folder
+copy package.json dist
+cd dist
+npm publish --access public
+
