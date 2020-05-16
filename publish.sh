64541f816b338aa6ae482ab41a5b4f61dbede1e0
SergeyMell <sergey.mell@agilie.com>
2020-05-16 23:21:37 +0300

    Fix publishing script

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ set -e # Stop running on exception
 # Update package version
 echo 'Put version [<newversion> | major | minor | patch | premajor | preminor | prepatch ]'
 read VERSION
-VERSION="${VERSION:-minor}" # defaults to minor
+VERSION="${VERSION:-patch}" # defaults to patch
 npm version $VERSION
 
 # Running tests and building the project
@@ -13,7 +13,7 @@ ng test --no-watch --code-coverage
 ng build
 
 # Send coverage report to codecov for github badge
-bash <(curl -s https://codecov.io/bash) -t f27c863b-d1a1-4e34-a7cf-5c4559de19f3
+curl -s https://codecov.io/bash | bash -s -- -t f27c863b-d1a1-4e34-a7cf-5c4559de19f3
 
 # Publishing the project from dist folder
 copy package.json dist
